78d0399fa867f159d1891b2d0bce9617ab8ff5e5
isaias7 <isaiassalas@MacBook-Pro-de-Isaias.local>
2017-05-16 15:24:33 -0600

    initial configuration

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,305 @@
 import React from 'react';
 import { render } from 'react-dom';
+import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import './scss/app.scss';
 
 const App = React.createClass({
   render() {
     return (
-      <div className='app'>
-        <div className='landing'>
-          <h1>OLA K ASE</h1>
+      <div className='container-fluid'>
+        <div className='row' id='mainView'>
+          {/*<!--MENU AZUL-->*/}
+          <div className='col-md-1 cols'>
+            <nav className='section sideNav'>
+              <ul>
+                <li>
+                  <button className='sideNav__button' id='user'>
+                    <img className='sideNav__image' src='../iconos/user-3.svg' title='User configurations' />
+                  </button>
+                </li>
+                <li>
+                  <button className='sideNav__button' id='buttonNote'>
+                    <img className='sideNav__image' src='../iconos/file.svg' title='New note' />
+                  </button>
+                </li>
+                <li>
+                  <button className='sideNav__button' id='buttonNotebook'>
+                    <img className='sideNav__image' src='../iconos/agenda.svg' title='Notebooks' />
+                  </button>
+                </li>
+                <li>
+                  <button className='sideNav__button' id='buttonTag'>
+                    <img className='sideNav__image' src='../iconos/tag.svg' title='Tags' />
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          {/*<!--TERMINA MENU AZUL-->*/}
+<div className="col-md-11">
+          {/*<!--NOTAS-->*/}
+
+          <section className='section notesDropDown'>
+            <div className='col-md-12 cols'>
+              <div className='row'>
+                <div className='col-md-12'>
+                  <select className='notesDropDown__dropdown'>
+                    <option value=''>BrainStation</option>
+                    <option value=''>UCR</option>
+                    <option value=''>ULACIT</option>
+                  </select>
+                  <button className='notesDropDrown__button' id='buttonTag'>
+                    <img className='notesDropDrown__img' src='../iconos/icon.svg' title='UNotes' />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+          <section className='section notes'>
+            <div className='row' id='thumbnails'>
+              <div className='col-md-3'>
+                <div className='thumbnail'>
+                  <div className='caption'>
+                    <h3>Nota 1</h3>
+                    <p className='p--shorter'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum obcaecati, hic commodi modi, consequuntur,
+                    sequi et incidunt necessitatibus, id ipsum reprehenderit inventore odio. Consequatur incidunt ipsam,
+                    esse recusandae iste sunt.</p>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/send.svg' title='Send' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/pencil.svg' title='Edit' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/delete.svg' title='Delete' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-md-3'>
+                <div className='thumbnail'>
+                  <div className='caption'>
+                    <h3>Nota 2</h3>
+                    <p className='p--shorter'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum obcaecati, hic commodi modi, consequuntur,
+                    sequi et incidunt necessitatibus, id ipsum reprehenderit inventore odio. Consequatur incidunt ipsam,
+                    esse recusandae iste sunt.</p>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/send.svg' title='Send' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/pencil.svg' title='Edit' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/delete.svg' title='Delete' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-md-3'>
+                <div className='thumbnail'>
+                  <div className='caption'>
+                    <h3>Nota 3</h3>
+                    <p className='p--shorter'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum obcaecati, hic commodi modi, consequuntur,
+                    sequi et incidunt necessitatibus, id ipsum reprehenderit inventore odio. Consequatur incidunt ipsam,
+                    esse recusandae iste sunt.</p>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/send.svg' title='Send' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/pencil.svg' title='Edit' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/delete.svg' title='Delete' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-md-3'>
+                <div className='thumbnail'>
+                  <div className='caption'>
+                    <h3>Nota 4</h3>
+                    <p className='p--shorter'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum obcaecati, hic commodi modi, consequuntur,
+                    sequi et incidunt necessitatibus, id ipsum reprehenderit inventore odio. Consequatur incidunt ipsam,
+                    esse recusandae iste sunt.</p>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/send.svg' title='Send' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/pencil.svg' title='Edit' />
+                    </button>
+                    <button className='note__button' href='#' id='buttonLib'>
+                      <img className='note__image' src='../iconos/delete.svg' title='Delete' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
+        </div>
+        {/*<!--TERMINA NOTAS-->*/}
+
+        {/*<!--CODIGO VIEJO-->*/}
+        {/*<!--NOTA-->*/}
+        <section id='notesModal' className='notesModal'>
+          <div className='row'>
+            {/*<!--menu notas-->*/}
+            <div className='col-md-12'>
+              <nav className='section navNote'>
+                <ul className='navNote__ul'>
+                  <li>
+                    <input className='form-control navNote__input' id='nameNote' type='text' placeholder='Title' />
+                  </li>
+                  <li>
+                    <button className='navNote__button' href='#'>
+                      <img className='navNote__image' src='../iconos/user-2.svg' title='Add contribuitor' />
+                    </button>
+                  </li>
+                  <li>
+                    <button className='navNote__button' a href='#'>
+                      <img className='navNote__image' src='../iconos/send.svg' title='Send' />
+                    </button>
+                  </li>
+                  <li>
+                    <button className='navNote__button' href='#'>
+                      <img className='navNote__image' src='../iconos/delete.svg' title='Delete' />
+                    </button>
+                  </li>
+                  <li>
+                    <button className='navNote__button' href='#'>
+                      <img className='navNote__image' src='../iconos/info-button.svg' title='Information' />
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            {/*<!--fin menu de notas-->*/}
+            {/*<!--menu editor de notas-->*/}
+            <div className='col-md-12'>
+              <nav className='navEditor'>
+                <ul className='navEditor__ul section'>
+                  <li>
+                    <button className='navEditor__button' href='#'>
+                      <img className='navEditor__image' src='../iconos/agenda.svg' title='Notebook' />
+                    </button>
+                  </li>
+                  <li>
+                    <select className='navEditor__dropdown'>
+                      <option value=''>BrainStation</option>
+                      <option value=''>UCR</option>
+                      <option value=''>ULACIT</option>
+                    </select>
+                  </li>
+                  <li>
+                    <button className='navEditor__button' href='#'>
+                      <img className='navEditor__image' src='../iconos/tag.svg' title='Tags' />
+                    </button>
+                  </li>
+                  <li>
+                    <input className='navEditor__input form-control' type='text' placeholder='Tag' />
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            {/*<!--fin menu editor de notas-->*/}
+            {/*<!--text area-->*/}
+            <div className='row'>
+              <div className='col-md-12'>
+                <section className='textArea'>
+                  <textarea className='textArea__input' type='text' rows='15' id='contentNote' placeholder='Start your note here...'></textarea>
+                </section>
+              </div>
+            </div>
+          </div>
+          {/*<!--fin textarea-->*/}
+        </section>
+        {/*<!--TERMINA NOTA-->*/}
+
+        {/*<!--MENU NOTEBOOKS-->*/}
+        <section className='sectionFile' id='sectionFile'>
+          <div className='row'>
+            <div className='col-md-11'>
+              <h2>Notebooks</h2>
+            </div>
+          </div>
+          <div className='row section'>
+            <input className='form-control' type='search' placeholder='Search' />
+            <button className='sectionFile__button sectionFile__button--margin' href='#' id='buttonEditar'>
+              <img id='imgEditar' className='sectionFile__image' src='../iconos/search.svg' title='Search' />
+            </button>
+          </div>
+          <ul id='notebooksList' className='notebooksList'>
+            <li className='sectionFile__list'>
+              <div className='row sectionFile__li'>
+                <div className='col-md-6'>
+                  <h6 contenteditable='true' id='inputName'>BrainStation</h6>
+                </div>
+                <div className='col-md-7'>
+                  <button className='sectionFile__button' href='#' id='buttonEditar'>
+                    <img id='imgEditar' className='sectionFile__image' src='../iconos/pencil.svg' title='Edit' />
+                  </button>
+                  <button className='sectionFile__button' href='#'>
+                    <img className='sectionFile__image' src='../iconos/file.svg' title='New note' />
+                  </button>
+                  <button className='sectionFile__button' href='#'>
+                    <img className='sectionFile__image' src='../iconos/delete.svg' title='Delete' />
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div className='row section'>
+            <input className='form-control' type='text' id='inputNoteBook' placeholder='New notebook' />
+            <button className='sectionFile__button sectionFile__button--margin' href='#' id='addNoteBook'>
+              <img className='sectionFile__image' src='../iconos/add.svg' title='Add' />
+            </button>
+          </div>
+        </section>
+        {/*<!--TERMINA MENU NOTEBOOK-->*/}
+
+        {/*<!--TERMINA TAGS-->*/}
+        {/*<!--AQUI DEBERIA EL DE TAGS-->*/}
+        <section className='tagFile' id='tagFile'>
+          <div className='row'>
+            <div className='col-md-11'>
+              <h2>Tags</h2>
+            </div>
+          </div>
+          <div className='row section'>
+            <input className='form-control' type='search' placeholder='Search' />
+            <button className='tagFile__button tagFile__button--margin' href='#' id='buttonEditar'>
+              <img id='imgEditar' className='tagFile__image' src='../iconos/search.svg' title='Search' />
+            </button>
+          </div>
+          <ul id='tagList' className='tagsList'>
+            <li className='tagFile__list'>
+              <div className='row tagFile__li'>
+                <div className='col-md-6'>
+                  <h6 contenteditable='true' id='inputTag'>HTML</h6>
+                </div>
+                <div className='col-md-7'>
+                  <button className='tagFile__button' href='#' id='buttonEditar'>
+                    <img id='imgEditar' className='tagFile__image' src='../iconos/pencil.svg' title='Edit' />
+                  </button>
+                  <button className='tagFile__button' href='#'>
+                    <img className='tagFile__image' src='../iconos/delete.svg' alt='Delete' />
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div className='row section'>
+            <input className='form-control' type='text' id='inputNewTag' placeholder='New tags' />
+            <button className='tagFile__button tagFile__button--margin' href='#' id='addTag'>
+              <img className='tagFile__image' src='../iconos/add.svg' title='Add' />
+            </button>
+          </div>
+        </section>
       </div>
     );
   },
---
+++ src/js/home.js
@@ -0,0 +1,154 @@
+(function(){
+
+document.getElementById('buttonNote').addEventListener('click',showNotesEditor);
+document.getElementById("buttonNotebook").addEventListener("click",showMenuNotebooks);
+document.getElementById("addNoteBook").addEventListener("click",addNoteBook);
+document.getElementById("buttonTag").addEventListener("click",showMenuTag);
+// document.getElementById("addTag").addEventListener("click",addNote);
+
+notesModal = document.getElementById("notesModal");
+notebooks = document.getElementById("sectionFile");
+inputNameNoteBook= document.getElementById("inputNoteBook");
+tags = document.getElementById('tagFile');
+
+var creatingNote=false;
+
+function showNotesEditor() {
+    tags.classList.remove('tagFile--show');
+    notesModal.classList.add("notesModal--show");
+    document.getElementById("nameNote").focus();
+    document.getElementById("mainView").classList.add("mainViewOpaque");
+    notebooks.classList.remove("sectionFile--show")
+    creatingNote =true;
+}
+document.onkeydown = function myFunction(event) {
+    switch(event.keyCode){
+        case 27:
+            notesModal.classList.remove('notesModal--show');
+            notebooks.classList.remove('sectionFile--show');
+            tags.classList.remove('tagFile--show');
+            document.getElementById('mainView').classList.remove('mainViewOpaque');
+            creatingNote=false;
+            document.getElementById('nameNote').value=''
+            document.getElementById('contentNote').value='';
+            break;
+        case 13:
+            createNote();
+            document.getElementById('nameNote').value=''
+            document.getElementById('contentNote').value='';
+    }
+}
+function createNote(){
+    if(creatingNote){
+        var title = document.getElementById('nameNote').value;
+        var content =document.getElementById('contentNote').value;
+
+        var h3 = document.createElement('h3');
+        h3.appendChild(document.createTextNode(title));
+
+        var p = document.createElement('p');
+        p.setAttribute('class','p--shorter');
+        p.appendChild(document.createTextNode(content));
+        
+        
+        var button1 = document.createElement('button');
+        var img1 =document.createElement('img');
+
+        img1.setAttribute('class','note__image');
+        img1.setAttribute('src','../iconos/send.svg');
+
+        button1.setAttribute('class','note__button');
+        
+        var button2 = document.createElement('button');
+        var img2 =document.createElement('img'); 
+        
+        img2.setAttribute('class','note__image');
+        img2.setAttribute('src','../iconos/pencil.svg');
+
+        button2.setAttribute('class','note__button');
+
+        var button3 = document.createElement('button');
+        var img3 =document.createElement('img');
+        
+        img3.setAttribute('class','note__image');
+        img3.setAttribute('src','../iconos/delete.svg');
+
+        button3.setAttribute('class','note__button');
+
+        button1.appendChild(img1);
+        button2.appendChild(img2);
+        button3.appendChild(img3);
+
+        //CAPTION
+        var div1 =document.createElement('div');
+        div1.setAttribute('class','caption');
+        var div2=document.createElement('div');
+        div2.setAttribute('class','thumbnail');
+        var div3=document.createElement('div');
+        div3.setAttribute('class', 'col-md-3');
+
+        div1.appendChild(h3);
+        div1.appendChild(p);
+
+        //buttons
+        div1.appendChild(button1);
+        div1.appendChild(button2);
+        div1.appendChild(button3);
+
+        div2.appendChild(div1);
+        div3.appendChild(div2);
+        //thumbnails
+        document.getElementById('thumbnails').appendChild(div3);
+        console.log(title+'-'+content);
+
+        notesModal.classList.remove('notesModal--show');
+        document.getElementById('mainView').classList.remove('mainViewOpaque');
+    }
+}
+function showMenuNotebooks(){
+    tags.classList.remove('tagFile--show');
+    notebooks.classList.add('sectionFile--show');
+    notesModal.classList.remove('notesModal--show');
+    tags.classList.remove('tagFile--show');
+    document.getElementById('mainView').classList.add('mainViewOpaque');
+}
+function addNoteBook(){
+    console.log('new book');
+
+    //TITULO DEL NOTEBOOK
+    var name =inputNameNoteBook.value;
+    var h6 = document.createElement('h6');
+    h6.setAttribute('contenteditable', 'true');
+    
+    var text = document.createTextNode(name);
+    h6.appendChild(text);
+
+    //ROW
+    var div = document.createElement('div');
+    div.setAttribute('class', 'row sectionFile__li');
+
+    //COLUMN
+    var divCol = document.createElement('div');
+    divCol.setAttribute('class', 'col-md-12');
+
+    div.appendChild(divCol);
+    divCol.appendChild(h6);
+
+    //LISTA
+    var node = document.createElement('li');
+    node.setAttribute('class', 'sectionFile__list');
+
+    node.appendChild(div);
+    document.getElementById('notebooksList').appendChild(node);
+    console.log('agregado');
+}
+// TAGS ---------------------------------------------------------------------
+function showMenuTag(){
+    console.log('hola');
+    tags.classList.add('tagFile--show');
+    notesModal.classList.remove('notesModal--show');
+    notebooks.classList.remove('sectionFile--show');
+    document.getElementById('mainView').classList.add('mainViewOpaque');
+}
+
+})();
