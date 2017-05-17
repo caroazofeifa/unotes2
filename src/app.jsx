b44951aefe6176991a493fbc3fe71b811ae014ae
Carolina Azofeifa <mcazofeifa@gmail.com>
2017-05-17 10:39:54 -0600

    New component Notebooks

--- src/app.jsx
+++ src/app.jsx
@@ -6,8 +6,8 @@ import './scss/app.scss';
 const NavMenu = require('./NavMenu');
 const DropDown = require('./DropDown');
 const Notes = require('./Notes');
-
 const EditorNotes = require('./EditorNotes');
+const Notebooks = require('./Notebooks');
 
 const App = React.createClass({
   render() {
@@ -33,45 +33,7 @@ const App = React.createClass({
         {/*<!--TERMINA NOTA-->*/}
 
         {/*<!--MENU NOTEBOOKS-->*/}
-        <section className='sectionFile' id='sectionFile'>
-          <div className='row'>
-            <div className='col-md-11'>
-              <h2>Notebooks</h2>
-            </div>
-          </div>
-          <div className='row section'>
-            <input className='form-control' type='search' placeholder='Search' />
-            <button className='sectionFile__button sectionFile__button--margin' href='#' id='buttonEditar'>
-              <img id='imgEditar' className='sectionFile__image' src='../iconos/search.svg' title='Search' />
-            </button>
-          </div>
-          <ul id='notebooksList' className='notebooksList'>
-            <li className='sectionFile__list'>
-              <div className='row sectionFile__li'>
-                <div className='col-md-6'>
-                  <h6 contenteditable='true' id='inputName'>BrainStation</h6>
-                </div>
-                <div className='col-md-7'>
-                  <button className='sectionFile__button' href='#' id='buttonEditar'>
-                    <img id='imgEditar' className='sectionFile__image' src='../iconos/pencil.svg' title='Edit' />
-                  </button>
-                  <button className='sectionFile__button' href='#'>
-                    <img className='sectionFile__image' src='../iconos/file.svg' title='New note' />
-                  </button>
-                  <button className='sectionFile__button' href='#'>
-                    <img className='sectionFile__image' src='../iconos/delete.svg' title='Delete' />
-                  </button>
-                </div>
-              </div>
-            </li>
-          </ul>
-          <div className='row section'>
-            <input className='form-control' type='text' id='inputNoteBook' placeholder='New notebook' />
-            <button className='sectionFile__button sectionFile__button--margin' href='#' id='addNoteBook'>
-              <img className='sectionFile__image' src='../iconos/add.svg' title='Add' />
-            </button>
-          </div>
-        </section>
+        <Notebooks />
         {/*<!--TERMINA MENU NOTEBOOK-->*/}
 
         {/*<!--TERMINA TAGS-->*/}
