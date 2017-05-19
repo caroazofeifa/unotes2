5ad9721584a8c67b0bbc85a38ff0bb2c0ac9c34f
isaias7 <isaiassalas@MBP-de-Isaias.telenetcr.net>
2017-05-19 00:25:51 -0600

    fix responsive errors

--- src/Tags.jsx
+++ src/Tags.jsx
@@ -5,41 +5,41 @@ const Tags = React.createClass({
   render() {
     return (
       <section className='tagFile' id='tagFile'>
-          <div className='row'>
-            <div className='col-md-11'>
-              <h2>Tags</h2>
-            </div>
+        <div className='row'>
+          <div className='col-md-11'>
+            <h2>Tags</h2>
           </div>
-          <div className='row section'>
-            <input className='form-control' type='search' placeholder='Search' />
-            <button className='tagFile__button tagFile__button--margin' href='#' id='buttonEditar'>
-              <img id='imgEditar' className='tagFile__image' src='../iconos/search.svg' title='Search' />
-            </button>
-          </div>
-          <ul id='tagList' className='tagsList'>
-            <li className='tagFile__list'>
-              <div className='row tagFile__li'>
-                <div className='col-md-6'>
-                  <h6 contenteditable='true' id='inputTag'>HTML</h6>
-                </div>
-                <div className='col-md-7'>
-                  <button className='tagFile__button' href='#' id='buttonEditar'>
-                    <img id='imgEditar' className='tagFile__image' src='../iconos/pencil.svg' title='Edit' />
-                  </button>
-                  <button className='tagFile__button' href='#'>
-                    <img className='tagFile__image' src='../iconos/delete.svg' alt='Delete' />
-                  </button>
-                </div>
+        </div>
+        <div className='row section'>
+          <input className='form-control tagFile__input' type='search' placeholder='Search' />
+          <button className='tagFile__button tagFile__button--margin' href='#' id='buttonEditar'>
+            <img id='imgEditar' className='tagFile__image' src='../iconos/search.svg' title='Search' />
+          </button>
+        </div>
+        <ul id='tagList' className='tagsList'>
+          <li className='tagFile__list'>
+            <div className='row tagFile__li'>
+              <div className='col-md-6'>
+                <h6 contenteditable='true' id='inputTag'>HTML</h6>
               </div>
-            </li>
-          </ul>
-          <div className='row section'>
-            <input className='form-control' type='text' id='inputNewTag' placeholder='New tags' />
-            <button className='tagFile__button tagFile__button--margin' href='#' id='addTag'>
-              <img className='tagFile__image' src='../iconos/add.svg' title='Add' />
-            </button>
-          </div>
-        </section>
+              <div className='col-md-7'>
+                <button className='tagFile__button' href='#' id='buttonEditar'>
+                  <img id='imgEditar' className='tagFile__image' src='../iconos/pencil.svg' title='Edit' />
+                </button>
+                <button className='tagFile__button' href='#'>
+                  <img className='tagFile__image' src='../iconos/delete.svg' alt='Delete' />
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div className='row section'>
+          <input className='form-control tagFile__input' type='text' id='inputNewTag' placeholder='New tags' />
+          <button className='tagFile__button tagFile__button--margin' href='#' id='addTag'>
+            <img className='tagFile__image' src='../iconos/add.svg' title='Add' />
+          </button>
+        </div>
+      </section>
     );
   },
 });
