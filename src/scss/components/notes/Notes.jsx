7fbe1ff1a9ac8df376727b2b0635be0a2a74f49d
isaias7 <isaiassalas@MacBook-Pro-de-Isaias.local>
2017-05-22 08:33:52 -0600

    fix notes editor menu

--- src/scss/components/notes/Notes.jsx
+++ src/scss/components/notes/Notes.jsx
@@ -21,3 +21,61 @@ const Notes = React.createClass({
 });
 
 module.exports = Notes;
+
+
+/*import React from 'react';
+import './_notes.scss';
+import preload from '../../../../public/unotes.json';
+
+const CardNotes = require('../cardNotes/CardNotes');
+
+const Notes = React.createClass({
+  render() {
+    return (
+      <section className='section notes'>
+        <div className='row' id='thumbnails'>
+          {preload.notebooks.note.map((note) => {
+            return (
+              <CardNotes key={ note.idNote } note={ note } />
+            );
+          })
+          }
+        </div>
+      </section>
+    );
+  },
+});
+
+module.exports = Notes;*/
+
+
+/*import React from 'react';
+import './_notes.scss';
+import preload from '../../../../public/unotes.json';
+
+const CardNotes = require('../cardNotes/CardNotes');
+
+const Notes = React.createClass({
+  render() {
+    return (
+      <div>
+      {preload.notebooksmap((notes) => {
+        return (
+          render () {
+            return {
+              <div>
+              preload.notes.map ((nota) => {
+                <CardNotes key={ nota.idNote } nota={ nota } />
+              })
+              </div>
+            }
+          }
+        );
+      })
+      </div>
+    }
+    );
+  },
+});
+
+module.exports = Notes;*/
